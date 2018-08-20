6e6b9e603162530bd34b43f93b324beb2399ad96
Tomas Hubelbauer <Tomas@Hubelbauer.net>
2018-08-20 11:25:57 +0200

    Scaffold a new single view app from XCode template

---
+++ Day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day
+//
+//  Created by Tomas Hubelbauer on 20/08/2018.
+//  Copyright © 2018 Tomas Hubelbauer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
